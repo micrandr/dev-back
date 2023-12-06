a3a1413215541ec45f2a6c128c85b5bb2ef5c359
Fy Dev <finoanamail@gmail.com>
2023-12-06 11:23:18 +0300

    update: vercel CORS authorization

--- src/components/DataCreateUser.tsx
+++ src/components/DataCreateUser.tsx
@@ -267,7 +267,7 @@ const DataCreateUser = () => {
                                     id="user-mobile"
                                     onChange={(e) => setUserMobile(e.target.value)}
                                     value={userMobile}
-                                    placeholder="Téléphone"
+                                    placeholder="Mobile"
                                     className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                                     required
                                 />
