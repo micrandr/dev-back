540ac852f955df397b8b184dca98df285f2c8d7d
Mickael <micrandr@gmail.com>
2024-01-18 09:17:44 +0300

    update: dashbaord

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -21,8 +21,8 @@ const ECommerce = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
+        {/* <ChartOne />
+        <ChartTwo /> */}
         <div className="col-span-12 xl:col-span-8">
           <DataListUsers />
         </div>
