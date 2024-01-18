6e3a71d71a2f340545220398b29fd88ebb945859
Mickael <micrandr@gmail.com>
2024-01-18 09:01:13 +0300

    change: url yigo

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -4,13 +4,13 @@ import axios from 'axios';
 export const API_URI_AXIOS = 'https://dev-api.formationbtp.fr/index.php/api/'
 
 //export const API_URI_ROOT = 'https://127.0.0.1:8000/api/'
-export const API_URI_ROOT = 'https://dev-api.formationbtp.fr/'
+export const API_URI_ROOT = 'http://devapi-of.yigo3928.odns.fr/'
 
 //export const API_URI_UPLOAD = 'https://127.0.0.1:8000/uploads/'
-export const API_URI_UPLOAD = 'https://dev-api.formationbtp.fr/uploads/'
+export const API_URI_UPLOAD = 'http://devapi-of.yigo3928.odns.fr/uploads'
 
 //export const API_URI_IMAGES = 'https://127.0.0.1:8000/images/'
-export const API_URI_IMAGES = 'https://dev-api.formationbtp.fr/images/'
+export const API_URI_IMAGES = 'http://devapi-of.yigo3928.odns.fr/images/'
 
 
 
