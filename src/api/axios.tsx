2240365b6ac72e4b9b7b7ebd08d00dd4ae9e89fc
Mickael <micrandr@gmail.com>
2024-01-18 08:40:32 +0300

    update: uri axios

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -3,15 +3,15 @@ import axios from 'axios';
 //export const API_URI_AXIOS = 'https://127.0.0.1:8000/api/'
 export const API_URI_AXIOS = 'https://dev-api.formationbtp.fr/index.php/api/'
 
-// export const API_URI_UPLOAD = 'https://127.0.0.1:8000/uploads/'
+//export const API_URI_UPLOAD = 'https://127.0.0.1:8000/uploads/'
 export const API_URI_UPLOAD = 'https://dev-api.formationbtp.fr/uploads/'
 
-// export const API_URI_IMAGES = 'https://127.0.0.1:8000/images/'
+//export const API_URI_IMAGES = 'https://127.0.0.1:8000/images/'
 export const API_URI_IMAGES = 'https://dev-api.formationbtp.fr/images/'
 
 
 
 export default axios.create({
-    baseURL: 'https://127.0.0.1:8000/api/'
-    // baseURL: 'https://dev-api.formationbtp.fr/index.php/api/'
+    //baseURL: 'https://127.0.0.1:8000/api/'
+    baseURL: 'https://dev-api.formationbtp.fr/index.php/api/'
 });
